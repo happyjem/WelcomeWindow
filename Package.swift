dd691979ce585b544c69a8f306974dc42940998c
Khan Winter <35942988+thecoolwinter@users.noreply.github.com>
2025-06-11 13:38:14 -0500

    Fix Lint Errors

--- Package.swift
+++ Package.swift
@@ -10,7 +10,8 @@ let package = Package(
     products: [
         .library(
             name: "WelcomeWindow",
-            targets: ["WelcomeWindow"]),
+            targets: ["WelcomeWindow"]
+        ),
     ],
     dependencies: [
         .package(
